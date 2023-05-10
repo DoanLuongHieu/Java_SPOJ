ea29c78dfdd0d1c153e0ebde757d145e770b5614
unknown <doanluonghieudlh@gmail.com>
2023-05-10 21:19:05 +0700

    Update BRIDGE.java

--- BRIDGE.java
+++ BRIDGE.java
@@ -1,4 +1,5 @@
 package Java_SPOJ;
+// Import các thư viện cần thiết để ghi và đọc dữ liệu từ đầu vào/đầu ra
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -6,13 +7,15 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.InputMismatchException;
+// Tạo lớp BRIDGE
 class BRIDGE {
+    // Tạo lớp Point, kế thừa từ interface Comparator
     static class Point implements Comparator<Point> {
+        // 2 thuộc tính x, y là tọa độ của 1 điểm
         int x, y;
-
         Point() {
         }
-
+        // Phương thức so sánh 2 điểm dựa trên tọa độ y. Nếu tọa độ y bằng nhau thì so sánh dựa trên tọa độ x
         @Override
         public int compare(Point o1, Point o2) {
             if (o1.y == o2.y)
@@ -22,40 +25,55 @@ class BRIDGE {
     }
 
     public static void main(String[] args) throws IOException {
+        // Tạo đối tượng sc từ lớp AmitScan
         AmitScan sc = new AmitScan();
+        // Tạo đối tượng pr từ lớp AmitPrint
         AmitPrint pr = new AmitPrint();
+        // Đọc số lượng test case từ đầu vào
         int t = sc.si();
+        // Vòng lặp duyệt qua từng test case
         while (t-- > 0) {
+            // Đọc số lượng điểm từ đầu vào
             int n = sc.si();
+            // Tạo mảng a chứa các điểm
             Point[] a = new Point[n];
             for (int i = 0; i < n; i++) {
                 a[i] = new Point();
+                // Đọc tọa độ x của điểm thứ i
                 a[i].x = sc.si();
             }
             for (int i = 0; i < n; i++)
+                // Đọc tọa độ y của điểm thứ i
                 a[i].y = sc.si();
-            // Sort according to y coordinates
+            // Sắp xếp mảng a dựa trên tọa độ y (sử dụng phương thức compare của lớp Point)
             Arrays.sort(a, new Point());
 
-            // LIS on x coordinates
+            // Tìm Longest Increasing Subsequence (LIS) trên tọa độ x
             int[] memo = new int[n];
+            // Khởi tạo từng phần tử của mảng memo bằng 1
             for (int i = 0; i < n; i++)
                 memo[i] = 1;
+            // Biến ans lưu giá trị LIS tối đa
             int ans = 1;
             for (int i = 1; i < n; i++) {
                 for (int j = 0; j < i; j++) {
+                    // Nếu tọa độ x của điểm j nhỏ hơn hoặc bằng tọa độ x của điểm i
                     if (a[j].x <= a[i].x) {
+                        // Cập nhật giá trị LIS tạm thời tại điểm i
                         int temp = 1 + memo[j];
                         memo[i] = Math.max(temp, memo[i]);
                     }
                 }
+                // Cập nhật giá trị LIS tối đa
                 ans = Math.max(ans, memo[i]);
             }
+            // In giá trị LIS tối đa ra đầu ra
             pr.pl(ans);
         }
+        // Đóng đầu ra
         pr.close();
     }
-
+    // Tạo lớp AmitScan để đọc dữ liệu từ đầu vào
     static class AmitScan {
         private byte[] buf = new byte[131072]; // Buffer of Bytes
         private int index;
@@ -66,7 +84,7 @@ class BRIDGE {
             in = System.in;
         }
 
-        private int scan() throws IOException // Scan method used to scan buf
+        private int scan() throws IOException // Phương thức scan để quét buffer
         {
             if (total < 0)
                 throw new InputMismatchException();
@@ -82,10 +100,10 @@ class BRIDGE {
         int si() throws IOException {
             int integer = 0;
             int n = scan();
-            while (isWhiteSpace(n)) // Removing starting whitespaces
+            while (isWhiteSpace(n)) // Xóa các khoảng trắng đầu
                 n = scan();
             int neg = 1;
-            if (n == '-') // If Negative Sign encounters
+            if (n == '-') // Nếu gặp dấu trừ
             {
                 neg = -1;
                 n = scan();
@@ -105,23 +123,19 @@ class BRIDGE {
             return n == ' ' || n == '\n' || n == '\r' || n == '\t' || n == -1;
         }
     }
-
+// Tạo lớp AmitPrint để ghi dữ liệu ra đầu ra
     static class AmitPrint {
         private final BufferedWriter bw;
-
         AmitPrint() {
             this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
         }
-
         private void p(Object object) throws IOException {
             bw.append("").append(String.valueOf(object));
         }
-
         void pl(Object object) throws IOException {
             p(object);
             bw.append("\n");
         }
-
         void close() throws IOException {
             bw.close();
         }
