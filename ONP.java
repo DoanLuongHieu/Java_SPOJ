7e8c78b8b23bd24e4ac4f4b52b55f5597463d837
unknown <doanluonghieudlh@gmail.com>
2023-05-10 17:12:13 +0700

    Update ONP.java

--- ONP.java
+++ ONP.java
@@ -3,46 +3,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+
 class ONP {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //đọc input từ bàn phím
+        String s = br.readLine(); //đọc số lượng biểu thức cần xử lý
         int T = Integer.parseInt(s);
-        while(T >0){
+        while (T > 0) { //vòng lặp để xử lý các biểu thức theo yêu cầu
             T--;
-            s = br.readLine();
-            Stack<Character> stack = new Stack<Character>();
-            StringBuilder op = new StringBuilder();
-            for(int i=0;i<s.length();i++){
+            s = br.readLine(); //đọc biểu thức từ bàn phím
+            Stack<Character> stack = new Stack<Character>(); //tạo một stack để lưu trữ toán tử
+            StringBuilder op = new StringBuilder(); //tạo một StringBuilder để lưu trữ các phần tử trong biểu thức kết quả
+            for (int i = 0; i < s.length(); i++) { //duyệt từng phần tử trong biểu thức
                 char current = s.charAt(i);
-                if(current  >= 'a' && current <= 'z'){
+                if (current >= 'a' && current <= 'z') { //nếu là chữ cái thì thêm vào biểu thức kết quả
                     op.append(current);
                     continue;
-                }
-                else if(current == '('){
+                } else if (current == '(') { //nếu là dấu mở ngoặc thì đẩy vào stack
                     stack.push(current);
                     continue;
-                }
-                else if(current == ')'){
-                    while(true){
+                } else if (current == ')') { //nếu là dấu đóng ngoặc thì lấy toán tử trong stack và thêm vào biểu thức kết quả cho đến khi gặp dấu mở ngoặc
+                    while (true) {
                         char temp = stack.pop();
-                        if(temp == '('){
+                        if (temp == '(') {
                             break;
-                        }else{
+                        } else {
                             op.append(temp);
                         }
                     }
                     continue;
-                }
-                else{
-                    while (true){
-                        if(stack.isEmpty()){
+                } else { //nếu là toán tử
+                    while (true) {
+                        if (stack.isEmpty()) { //nếu stack rỗng thì đẩy toán tử vào stack
                             stack.push(current);
                             break;
                         }
-                        if(!currentPrecedenceGreaterThanTop(current,stack.peek())){
+                        if (!currentPrecedenceGreaterThanTop(current, stack.peek())) { //nếu độ ưu tiên của toán tử hiện tại nhỏ hơn hoặc bằng độ ưu tiên của toán tử trên cùng của stack thì lấy toán tử đó ra và thêm vào biểu thức kết quả
                             op.append(stack.pop());
-                        }else{
+                        } else { //ngược lại, đẩy toán tử vào stack
                             stack.push(current);
                             break;
                         }
@@ -50,16 +48,17 @@ class ONP {
                     continue;
                 }
             }
-            while (!stack.empty()){
+            while (!stack.empty()) { //sau khi duyệt hết biểu thức, lấy các toán tử còn lại trong stack và thêm vào biểu thức kết quả
                 op.append(stack.pop());
             }
-            System.out.println(op.toString());
+            System.out.println(op.toString()); //in ra biểu thức kết quả
         }
     }
-    private static boolean currentPrecedenceGreaterThanTop(char current, Character peek) {
+
+    private static boolean currentPrecedenceGreaterThanTop(char current, Character peek) { //hàm kiểm tra độ ưu tiên của toán tử hiện tại có lớn hơn toán tử trên cùng của stack hay không
         String prec = "(+-*/^)";
         int cur = prec.indexOf(current);
         int top = prec.indexOf(peek);
-        return (cur>top);
+        return (cur > top);
     }
 }
