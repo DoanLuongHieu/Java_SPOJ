c1f733eee079cf71f33f51c72960cbf762740cd3
unknown <doanluonghieudlh@gmail.com>
2023-05-10 20:30:40 +0700

    Update SOCNETC.java

--- SOCNETC.java
+++ SOCNETC.java
@@ -2,7 +2,7 @@ package Java_SPOJ;
 import java.io.*;
 import java.util.*;
 class SOCNETC {
-    static class Reader {
+    static class Reader { // Lớp Reader dùng để đọc đầu vào từ một file hoặc từ bàn phím
         final private int BUFFER_SIZE = 1 << 16;
         private final DataInputStream din;
         private final byte[] buffer;
@@ -17,7 +17,7 @@ class SOCNETC {
             buffer = new byte[BUFFER_SIZE];
             bufferPointer = bytesRead = 0;
         }
-        public String readLine() throws IOException {
+        public String readLine() throws IOException { // Đọc một dòng từ đầu vào
             byte[] buf = new byte[16]; // line length
             int cnt = 0, c;
             while ((c = read()) != -1) {
@@ -28,7 +28,7 @@ class SOCNETC {
             }
             return new String(buf, 0, cnt);
         }
-        public int nextInt() throws IOException {
+        public int nextInt() throws IOException { // Đọc một số nguyên từ đầu vào
             int ret = 0;
             byte c = read();
             while (c <= ' ') {
@@ -46,54 +46,56 @@ class SOCNETC {
             }
             return ret;
         }
-        private void fillBuffer() throws IOException {
+        private void fillBuffer() throws IOException { // Đọc các byte từ đầu vào và đưa vào buffer
             bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
             if (bytesRead == -1) {
                 buffer[0] = -1;
             }
         }
-        private byte read() throws IOException {
+        private byte read() throws IOException { // Đọc một byte từ buffer và đưa vào đầu ra
             if (bufferPointer == bytesRead) {
                 fillBuffer();
             }
             return buffer[bufferPointer++];
         }
-        public void close() throws IOException {
+        public void close() throws IOException { // Đóng đầu vào
             din.close();
         }
     }
+    // Hàm main
     public static void main(String[] args) throws IOException {
-        Reader reader = new Reader();
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] nm = reader.readLine().split(" ");
-        int n = Integer.parseInt(nm[0]);
-        int m = Integer.parseInt(nm[1]);
-        int[] parent = new int[n + 1];
-        int[] size = new int[n + 1];
+        Reader reader = new Reader(); // Tạo một đối tượng Reader
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // Tạo một đối tượng BufferedWriter để ghi đầu ra
+        String[] nm = reader.readLine().split(" "); // Đọc và tách chuỗi đầu vào thành mảng các chuỗi con
+        int n = Integer.parseInt(nm[0]); // Lấy giá trị n từ mảng nm
+        int m = Integer.parseInt(nm[1]); // Lấy giá trị m từ mảng nm
+        int[] parent = new int[n + 1]; // Mảng parent dùng để lưu trữ thông tin về các đỉnh cha trong một cây
+        int[] size = new int[n + 1]; // Mảng size dùng để lưu trữ kích thước của từng cây
         for (int i = 1; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
+            parent[i] = i; // Khởi tạo giá trị ban đầu cho mảng parent
+            size[i] = 1; // Khởi tạo giá trị ban đầu cho mảng size
         }
-        int numOfQueries = reader.nextInt();
+        int numOfQueries = reader.nextInt(); // Đọc số lượng truy vấn từ đầu vào
+        // Vòng lặp xử lý từng truy vấn
         while (numOfQueries-- > 0) {
-            String q = reader.readLine();
-            StringTokenizer str = new StringTokenizer(q);
-            String query = str.nextToken();
-            int x = Integer.parseInt(str.nextToken());
-            if (query.equals("S")) {
-                bw.write(size[findParent(parent, x)] + "\n");
+            String q = reader.readLine(); // Đọc một dòng truy vấn từ đầu vào
+            StringTokenizer str = new StringTokenizer(q); // Tách chuỗi truy vấn thành một mảng các token
+            String query = str.nextToken(); // Lấy token đầu tiên làm loại truy vấn
+            int x = Integer.parseInt(str.nextToken()); // Lấy token thứ hai làm giá trị x
+            if (query.equals("S")) { // Nếu truy vấn là S (size)
+                bw.write(size[findParent(parent, x)] + "\n"); // Ghi kích thước của cây có đỉnh x
             } else {
-                int y = Integer.parseInt(str.nextToken());
-                int parentX = findParent(parent, x);
-                int parentY = findParent(parent, y);
-                if (query.equals("A")) {
-                    if (parentX != parentY) {
-                        if (size[parentX] + size[parentY] <= m) {
-                            union(parent, size, parentX, parentY);
+                int y = Integer.parseInt(str.nextToken()); // Lấy token thứ ba làm giá trị y
+                int parentX = findParent(parent, x); // Tìm đỉnh cha của đỉnh x
+                int parentY = findParent(parent, y); // Tìm đỉnh cha của đỉnh y
+                if (query.equals("A")) { // Nếu truy vấn là A (add)
+                    if (parentX != parentY) { // Nếu x và y không cùng thuộc một cây
+                        if (size[parentX] + size[parentY] <= m) { // Nếu tổng kích thước của hai cây nhỏ hơn hoặc bằng m
+                            union(parent, size, parentX, parentY); // Hợp nhất hai cây
                         }
                     }
-                } else {
-                    if (parentX == parentY) {
+                } else { // Nếu truy vấn là Q (query)
+                    if (parentX == parentY) { // Nếu x và y cùng thuộc một cây
                         bw.write("Yes\n");
                     } else {
                         bw.write("No\n");
@@ -101,21 +103,23 @@ class SOCNETC {
                 }
             }
         }
-        bw.flush();
+        bw.flush(); // Đẩy dữ liệu đầu ra
     }
+    // Hàm hợp nhất hai cây
     private static void union(int[] parent, int[] size, int parentX, int parentY) {
         if (size[parentX] > size[parentY]) {
-            parent[parentY] = parentX;
-            size[parentX] += size[parentY];
+            parent[parentY] = parentX; // Đặt cha của cây y là x
+            size[parentX] += size[parentY]; // Cập nhật kích thước của cây x
         } else {
-            parent[parentX] = parentY;
-            size[parentY] += size[parentX];
+            parent[parentX] = parentY; // Đặt cha của cây x là y
+            size[parentY] += size[parentX]; // Cập nhật kích thước của cây y
         }
     }
+    // Hàm tìm đỉnh cha của một đỉnh
     private static int findParent(int[] parent, int x) {
-        if (parent[x] == x) {
-            return x;
+        if (parent[x] == x) { // Nếu x là đỉnh cha của nó
+            return x; // Trả về x
         }
-        return parent[x] = findParent(parent, parent[x]);
+        return parent[x] = findParent(parent, parent[x]); // Tìm đỉnh cha của x
     }
 }
